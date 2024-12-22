74e318075ec8193d4baa2af46a559e2a913e05ac
Skipper <155382339+Sklpp3r@users.noreply.github.com>
2024-12-22 04:05:52 +0300

    Rope 2

--- Assets/Scripts/Player/MagnetMovement.cs
+++ Assets/Scripts/Player/MagnetMovement.cs
@@ -5,32 +5,22 @@ public class MagnetMovement : MonoBehaviour
     [SerializeField] private Transform magnet1;
     [SerializeField] private Transform magnet2;
     [SerializeField] private float _fallSpeed;
-    
+
 
     private bool isSelected = false;
-
     private int sayi;
-    public bool dif;
 
     public float moveSpeed = 5f;
 
-    public GameObject camera;
+
 
     void Update()
     {
 
         transform.position += Vector3.down * _fallSpeed * Time.deltaTime;
-        
 
 
-        if (camera.activeSelf)
-        {
-            dif = true;
-        }
-        else
-        {
-            dif = false;
-        }
+
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
@@ -45,17 +35,17 @@ public class MagnetMovement : MonoBehaviour
         }
 
 
-        if (isSelected && dif == true)
+        if (isSelected == true)
         {
             if (sayi == 1)
             {
                 if (Input.GetKey(KeyCode.UpArrow))
                 {
-                    magnet1.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+                    magnet1.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.World);
                 }
                 if (Input.GetKey(KeyCode.DownArrow))
                 {
-                    magnet1.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+                    magnet1.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
                 }
             }
 
@@ -63,45 +53,20 @@ public class MagnetMovement : MonoBehaviour
             {
                 if (Input.GetKey(KeyCode.UpArrow))
                 {
-                    magnet2.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+                    magnet2.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.World);
                 }
 
                 if (Input.GetKey(KeyCode.DownArrow))
                 {
-                    magnet2.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+                    magnet2.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
                 }
             }
 
         }
 
-        if (isSelected && dif == false)
-        {
-            if (sayi == 1)
-            {
-                if (Input.GetKey(KeyCode.A))
-                {
-                    magnet1.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-                }
 
-                if (Input.GetKey(KeyCode.D))
-                {
-                    magnet1.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
-                }
-            }
 
-            if (sayi == 0)
-            {
-                if (Input.GetKey(KeyCode.A))
-                {
-                    magnet2.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-                }
 
-                if (Input.GetKey(KeyCode.D))
-                {
-                    magnet2.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
-                }
-            }
-        }
 
 
     }
